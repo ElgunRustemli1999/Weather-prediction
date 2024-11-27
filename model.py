eb399996d424b092269e47e4c6e6c0e959a82cbd
ElgunRustemli1999 <131358820+ElgunRustemli1999@users.noreply.github.com>
2024-11-27 10:06:08 +0300

    Update

--- model.py
+++ model.py
@@ -5,18 +5,57 @@ import functions
 import numpy as np
 import warnings
 from sklearn.preprocessing import MinMaxScaler
+import requests
+import os
+
+
+
+
 warnings.filterwarnings("ignore")
 def model_pred(city):
     city = city.lower()
-    model_path = f'C:/Users/Gold/Desktop/different_weather_app/models/{city}_data_model.keras'
+    def download_file(url, local_path):
+        response = requests.get(url)
+        with open(local_path, 'wb') as file:
+            file.write(response.content)
+
+    # Model URL ve veri URL'lerini GitHub'dan alın
+    model_url = f'https://github.com/ElgunRustemli1999/weather-prediction/tree/main/models/{city}_data_model.keras'
+    data_url = f'https://github.com/ElgunRustemli1999/weather-prediction/tree/main/data/{city}_data.csv'
+
+    # Dosyaların Heroku geçici dizinine indirilmesi
+    model_path = '/tmp/city_data_model.keras'
+    data_path = '/tmp/city_data.csv'
+
+    # GitHub'dan model ve veri dosyasını indir
+    download_file(model_url, model_path)
+    download_file(data_url, data_path)
+
+    # Modeli yükleme
     try:
         model = load_model(model_path)
         print("Model başarıyla yüklendi.")
     except Exception as e:
         print(f"Model yüklenirken hata oluştu: {e}")
-    data_path = f'C:/Users/Gold/Desktop/different_weather_app/data/{city}_data.csv'
 
-    data = functions.load_and_preprocess(data_path)
+    # Veri dosyasını okuma
+    try:
+        # Örneğin pandas ile veri dosyasını okuma
+        
+        data = functions.load_and_preprocess(data_path)
+        print("Veri başarıyla yüklendi.")
+    except Exception as e:
+        print(f"Veri yüklenirken hata oluştu: {e}")
+    # local ucun
+    # model_path = f'C:/Users/Gold/Desktop/different_weather_app/models/{city}_data_model.keras'
+    # try:
+    #     model = load_model(model_path)
+    #     print("Model başarıyla yüklendi.")
+    # except Exception as e:
+    #     print(f"Model yüklenirken hata oluştu: {e}")
+    #data_path = f'C:/Users/Gold/Desktop/different_weather_app/data/{city}_data.csv'
+
+    
 
     feature_columns = [col for col in data.columns if col !="tavg "]
     target_column = 'tavg'
